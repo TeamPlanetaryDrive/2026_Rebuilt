2f812ed1b7effe1a713bfb00aac467492f885329
Mileiky <yangmeilok@gmail.com>
2026-03-19 14:26:18 -0400

    bruh fixed moveback distance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -133,8 +133,8 @@ public final class Constants {
 
     public static final class AutoConstants {
         // acceleration and angular per second
-        public static final double kMoveBackDistance = 2.0; //meters
-        public static final double kMaxSpeedMetersPerSecond = 1;
+        public static final double kMoveBackDistance = 2.7; //meters
+        public static final double kMaxSpeedMetersPerSecond = 1.5;
         public static final double kMaxAccelerationMetersPerSecondSquared = 1;
         public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
         public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
