7fbcdd429c7e6ae0f510633cf91b57078f6405f8
kazzhi <katherinezhang2236@gmail.com>
2026-03-20 13:41:51 -0400

    pleaseeeee

--- src/main/java/frc/robot/commands/auto/MoveBackAndShoot.java
+++ src/main/java/frc/robot/commands/auto/MoveBackAndShoot.java
@@ -5,6 +5,8 @@ import java.util.List;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
@@ -33,36 +35,39 @@ public class MoveBackAndShoot extends Command {
         drive.disableVisionPoseUpdates();
         Pose2d start = drive.getPose();
 
-        Pose2d end = new Pose2d(
-            start.getX() - AutoConstants.kMoveBackDistance,
-            start.getY(),
-            start.getRotation()
-        );
+        
+        drive.resetOdometry(start);
+        // create endpoint
+        Pose2d end = start.transformBy(new Transform2d(-AutoConstants.kMoveBackDistance, 0.0, new Rotation2d()));
+
 
         TrajectoryConfig config = new TrajectoryConfig(
             AutoConstants.kMaxSpeedMetersPerSecond,
             AutoConstants.kMaxAccelerationMetersPerSecondSquared
         ).setKinematics(DriveConstants.kDriveKinematics);
 
+        // generate the trajectory
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
             List.of(start, end),
             config
         );
 
+
         ProfiledPIDController thetaController = new ProfiledPIDController(
             AutoConstants.kPThetaController,
             0,
-            0,
+            0.001,
             AutoConstants.kThetaControllerConstraints
         );
         thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
+        // set
         Command moveCommand = new SwerveControllerCommand(
             trajectory,
             drive::getPose,
             DriveConstants.kDriveKinematics,
-            new PIDController(AutoConstants.kPXController, 0, 0),
-            new PIDController(AutoConstants.kPYController, 0, 0),
+            new PIDController(AutoConstants.kPXController, 0, 0.001),
+            new PIDController(AutoConstants.kPYController, 0, 0.001),
             thetaController,
             drive::setModuleStates,
             drive
@@ -80,7 +85,7 @@ public class MoveBackAndShoot extends Command {
             Commands.waitSeconds(1.0),
             Commands.runOnce(shooter::feedForward, shooter),
 
-            Commands.waitSeconds(10.0)
+            Commands.waitSeconds(5.0)
 
 
         );
